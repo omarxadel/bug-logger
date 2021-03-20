76db62e2bc45e426e7dbd3e3ecd4813104b23de9
Omar Adel <48330931+omarxadel@users.noreply.github.com>
2021-03-20 17:17:47 +0200

    Add alert on adding logs

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { lchown } from 'original-fs'
 import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container'
 import Table from 'react-bootstrap/Table'
+import Alert from 'react-bootstrap/Alert'
 import LogItem from './LogItem'
 import AddLogItem from './AddLogItem'
 
@@ -29,16 +30,42 @@ const App = () => {
 			created: new Date().toString()
 		}
 	])
+	const [alert, setAlert] = useState({
+		show: false,
+		message: '',
+		variant: 'success'
+	})
 	
 	function addLog(item) {
+		if(item.text === '' || item.user === '' || item.priority === ''){
+			showAlert('Please complete log data!', 'danger')
+			return false
+		}
 		item._id = Math.floor(Math.random() * 90000) + 10000
 		item.created = new Date().toString()
 		setLogs([...logs, item])
+		showAlert('Log added successfully!')
+	}
+
+	function showAlert(message, variant = 'success', seconds = 3000){
+		setAlert({
+			show: true,
+			message,
+			variant
+		})
+		setTimeout(() => {
+			setAlert({
+				show: false,
+				message: '',
+				variant: 'success'
+			})
+		}, seconds)
 	}
 
 	return (
 		<Container>
 			<AddLogItem addLog={ addLog }/>
+			{alert.show && <Alert variant={alert.variant}>{ alert.message }</Alert>}
 			<Table >
 				<thead>
 					<tr>
